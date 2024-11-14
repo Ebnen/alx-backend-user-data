79661b88bb7af72c958431bc41a2b5a9d551720a
Ebnen <Ebenzy1996@gmail.com>
2024-11-14 17:23:25 +0100

    api

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,6 +5,13 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
+
+@app_views.route('/api/v1/unauthorized', methods=['GET'])
+def unauthorized_route():
+    """Endpoint to trigger a 401 error."""
+    abort(401)
+
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
