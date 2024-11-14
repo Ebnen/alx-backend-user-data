fc4bad518be435dab46d8baf3161eb26e4af61b1
Ebnen <Ebenzy1996@gmail.com>
2024-11-14 17:46:18 +0100

    api

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,7 +5,6 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
-
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
